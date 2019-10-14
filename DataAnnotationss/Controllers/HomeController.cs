9380e9179e7038f682d307a20d4d891ee44b1a0a
Shubhendra Srivastava <himanshusri1808@gmail.com>
2019-10-14 19:47:39 +0530

    AngularDatatable
    AngularDatatable

---
+++ DataAnnotationss/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAnnotationss.Models;
+namespace DataAnnotationss.Controllers
+{
+    public class HomeController : Controller
+    {
+        SampleMVCDBEntities da = new SampleMVCDBEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(UserTable ut)
+        {
+            da.UserTables.Add(ut);
+            da.SaveChanges();
+            return View();
+        }
+        public JsonResult GetUser()
+        {
+            return Json(da.UserTables.ToList(), JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public ActionResult DeleteUser(int Id)
+        {
+            var q = (da.UserTables.Where(k => k.Id == Id).FirstOrDefault());
+            if (q != null)
+            {
+                da.UserTables.Remove(q);
+                da.SaveChanges();
+            }
+            return new EmptyResult();
+        }
+        [HttpGet]
+        public ActionResult UpdateUser(int Id)
+        {
+            return Json(da.UserTables.Where(k => k.Id == Id).FirstOrDefault(), JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public ActionResult UpdateUser(UserTable ut)
+        {
+            var q = da.UserTables.Where(k => k.Id == ut.Id).FirstOrDefault();
+            q.Name = ut.Name;
+            q.Address = ut.Address;
+            q.Email = ut.Email;
+            q.Password = ut.Password;
+            da.SaveChanges();
+            return new EmptyResult();
+        }
+
+        [OutputCache(Duration = 20, Location = System.Web.UI.OutputCacheLocation.Any, NoStore = true, VaryByParam = "id")]
+        public ActionResult CachData(int id)
+        {
+            return Content("User Id= " + id + "Current Time= " + DateTime.Now.ToLongTimeString());
+        }
+    }
+}
---
+++ DataAnnotationss/Scripts/myjavascript.js
@@ -0,0 +1,86 @@
+﻿var q = angular.module("myapp", ['ui.bootstrap']);
+q.controller("adduser", function ($scope, $http) {
+    $scope.currentPage = 1;
+    $scope.so = 2;
+
+    $scope.InsertData = function (udata) {
+        $http.post('/Home/Index/', { ut: udata }).success(
+            function (response) {
+                alert('Success');
+                $scope.getdata();
+            }).error(
+                function (response) {
+                    alert('Error');
+                });
+    };
+    $scope.getdata = function () {
+        $http.get('/Home/GetUser').success(
+            function (response) {
+                $scope.users = response;
+            });
+    };
+    $http.get('/Home/GetUser').success(
+        function (response) {
+            $scope.users = response;
+        });
+    $scope.Delete = function (Id) {
+        $http.post('/Home/DeleteUser/', { Id: Id }).success(
+            function (response) {
+                alert('Success');
+                $scope.getdata();
+            }).error(
+                function (response) {
+                    alert('Error');
+                });
+    };
+    $scope.Update = function (Id) {
+        $http.get('/Home/UpdateUser/?Id=' + Id).success(
+            function (updata) {
+                $scope.updata.Id = updata.Id;
+                $scope.updata.Name = updata.Name;
+                $scope.updata.Address = updata.Address;
+                $scope.updata.Email = updata.Email;
+                $scope.updata.Password = updata.Password;
+            });
+    };
+    $scope.finalUpdate = function (updata) {
+        $http.post('/Home/UpdateUser/', { ut: updata }).success(
+            function (response) {
+                $('#modalid').modal('hide');
+                $scope.getdata();
+            }).error(
+                function (response) {
+                    alert('Error');
+                });
+    };
+
+
+
+    // sort ordering (Ascending or Descending). Set true for desending
+    $scope.reverse = false;
+
+    // called on header click
+    $scope.sortColumn = function (col) {
+        $scope.column = col;
+        if ($scope.reverse) {
+            $scope.reverse = false;
+            $scope.reverseclass = 'arrow-up';
+        } else {
+            $scope.reverse = true;
+            $scope.reverseclass = 'arrow-down';
+        }
+    };
+
+    // remove and change class
+    $scope.sortClass = function (col) {
+        if ($scope.column === col) {
+            if ($scope.reverse) {
+                return 'arrow-down';
+            } else {
+                return 'arrow-up';
+            }
+        } else {
+            return '';
+        }
+    };
+});
